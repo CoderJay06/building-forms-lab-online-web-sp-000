f46b880aaaff46c661509729b29f77fb9578842c
github username <jaycru06@yahoo.com>
2021-03-23 15:18:20 -0400

    commit tree a2325837e1bb93438b29bbecbf1dbd332e82edff

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -20,10 +20,11 @@ class BandInput extends Component {
   // update stores state when user hits submit
   handleOnSubmit = event => {
     event.preventDefault();
-    // debugger
+
     const band = {
       name: this.state.name
     }
+
     // dispatch action to update stores state
     this.props.addBand(band);
     this.setState({
@@ -35,29 +36,14 @@ class BandInput extends Component {
     return(
       <div>
         Band Input
-        {/* {console.log(this.props)} */}
         <form onSubmit={this.handleOnSubmit}>
           <input type="text" onChange={this.handleOnChange} value={this.state.name} />
           <br />
           <input type="submit" value="Add" />
         </form>
-        {this.state.name}
       </div>
     )
   }
 }
-
-// const mapStateToProps = state => {
-//   console.log(state)
-//   return {
-//     bands: state.bands
-//   }
-// }
-
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     addBand: band => dispatch({type: 'ADD_BAND', band: band})
-//   }
-// }
 
 export default BandInput; 
